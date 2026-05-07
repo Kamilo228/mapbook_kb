eda653401b0dc4c81b55c73c58f3e9590c1f78a7
Kamilo228 <kamil.blaszczyk01@student.wat.edu.pl>
2026-05-07 12:15:21 +0200

    feat: interactive map of users

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 from mapbook_lib.model import users
-from mapbook_lib.controler import read_users, add_user, remove_user, update_user, update_user_post
+
+from mapbook_lib.controler import read_users, add_user, remove_user, update_user, update_user_post, get_user_map
 
 def main():
     while True:
-        print('======MENU=======')
-        print('0 - Zakończ program')
-        print('1 - Wyświetl znajomych')
-        print('2 - Dodaj znajomego')
-        print('3 - Usuń znajowwego')
-        print('4 - Zaktualizuj znajowwego')
-        print('5 - Zaktualizuj posta')
-        choice = input('Wybierz opcję menu: ')
-        print(f'Wybrano opcję {choice}')
-        if choice == '0':
+        print("==========MENU==========")
+        print("0 - zakończ program")
+        print("1 - wyświetl znajomych")
+        print("2 - dodanie znajomego")
+        print("3 - usuwanie znajomego")
+        print("4 - update znajomego")
+        print("5 - update postów")
+        print("6 - mapa lokalizacji znajomych")
+
+        choice = input("Wybierz opcje w menu: ")
+        print(f"Wybrano opcję {choice}")
+        if choice == "0":
             break
 
-        if choice == '1':
+        if choice == "1":
             read_users(users)
-
-        if choice == '2':
+        if choice == "2":
             add_user(users)
-
-        if choice == '3':
+        if choice == "3":
             remove_user(users)
-
-        if choice == '4':
+        if choice == "4":
             update_user(users)
-
-        if choice == '5':
+        if choice == "5":
             update_user_post(users)
+        if choice == "6":
+            get_user_map(users)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
